af933bc60107d25ffc513f241df32aab5bce7c2c
Nashwan Azhari <nazhari@cloudbasesolutions.com>
2024-07-18 13:48:08 +0300

    Improve Docker image verification utilities
    Signed-off-by: Nashwan Azhari <nazhari@cloudbasesolutions.com>

--- k8s_test_harness/util/docker_util.py
+++ k8s_test_harness/util/docker_util.py
@@ -4,11 +4,40 @@
 #
 
 import subprocess
+from typing import List
 
 
 def ensure_image_contains_paths(image, paths):
-    for path in paths:
-        subprocess.run(
-            ["docker", "run", "--rm", image, "ls", "-l", path],
-            check=True,
-        )
+    """Ensures the given container image contains the provided paths
+    within it by attempting to run the image and 'ls' all of them.
+    """
+    base_cmd = ["docker", "run", "--rm", "--entrypoint", "ls", image, "-l"]
+    base_cmd.extend(paths)
+
+    subprocess.run(base_cmd, check=True)
+
+
+def list_files_under_container_image_dir(
+        image: str, root_dir: str="/", exclude_hidden_files: bool=True) -> List[str]:
+    """Lists all regular file paths under the given dir in the given image by
+    attempting to run the image and executing `find -type f` within the dir.
+    """
+    cmd = [
+        "docker",
+        "run",
+        "--rm",
+        "--entrypoint",
+        "find",
+        image,
+        root_dir.rstrip('/'),
+        "-type",
+        "f"
+    ]
+
+    if exclude_hidden_files:
+        cmd.extend(
+            ["-not", "-path", "'*/\\.*'", "(", "!", "-iname", ".*", ")"])
+
+    proc = subprocess.run(cmd, check=True, capture_output=True)
+
+    return [l.decode('utf8').strip() for l in proc.stdout.splitlines()]
